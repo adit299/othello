ff16198770afd3879cca557efc151ad7b33dd923
daniel.perevalov <daniel.perevalov@mail.utoronto.ca>
2019-10-27 19:57:22 -0400

    worked on 1.01

--- Assignment2/src/ca/utoronto/utm/othello/viewcontroller/OthelloApplication.java
+++ Assignment2/src/ca/utoronto/utm/othello/viewcontroller/OthelloApplication.java
@@ -1,7 +1,9 @@
 package ca.utoronto.utm.othello.viewcontroller;
 import ca.utoronto.utm.othello.model.Othello;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -26,6 +28,15 @@ public class OthelloApplication extends Application {
 		// MODEL->VIEW hookup
 		
 		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(5));
+		grid.setGridLinesVisible(true);
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
+				Button but = new Button(" ");
+				grid.add(but, col, row);
+			}
+		}
+		
 		
 		// SCENE
 		Scene scene = new Scene(grid); 
