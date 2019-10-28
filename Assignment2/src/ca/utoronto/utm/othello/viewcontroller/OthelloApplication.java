d74ddac68b6948b73edbad64af6f1f7162eeaac3
daniel.perevalov <daniel.perevalov@mail.utoronto.ca>
2019-10-28 13:39:39 -0400

    done us1.01

--- Assignment2/src/ca/utoronto/utm/othello/viewcontroller/OthelloApplication.java
+++ Assignment2/src/ca/utoronto/utm/othello/viewcontroller/OthelloApplication.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 public class OthelloApplication extends Application {
@@ -16,6 +18,7 @@ public class OthelloApplication extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		// Create and hook up the Model, View and the controller
+		CButtonPressEventHandler cbhandler;
 		
 		// MODEL
 		Othello othello=new Othello();
@@ -27,12 +30,19 @@ public class OthelloApplication extends Application {
 		// VIEW->CONTROLLER hookup
 		// MODEL->VIEW hookup
 		
+		// VIEW COMPONENTS
 		GridPane grid = new GridPane();
 		grid.setPadding(new Insets(5));
-		grid.setGridLinesVisible(true);
+		
+		Label lab = new Label();
+		grid.add(lab, 0, 8);
+		cbhandler = new CButtonPressEventHandler(lab);
+		
 		for (int row = 0; row < 8; row++) {
 			for (int col = 0; col < 8; col++) {
 				Button but = new Button(" ");
+				but.setId(String.valueOf(row) + ',' + String.valueOf(col));
+				but.setOnAction(cbhandler);
 				grid.add(but, col, row);
 			}
 		}
