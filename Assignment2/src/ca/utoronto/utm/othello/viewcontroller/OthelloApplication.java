f0282dc0df3c6af486cde8b004f2f6469f9d7105
daniel.perevalov <daniel.perevalov@mail.utoronto.ca>
2019-10-29 15:10:36 -0400

    testtest

--- Assignment2/src/ca/utoronto/utm/othello/viewcontroller/OthelloApplication.java
+++ Assignment2/src/ca/utoronto/utm/othello/viewcontroller/OthelloApplication.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 public class OthelloApplication extends Application {
