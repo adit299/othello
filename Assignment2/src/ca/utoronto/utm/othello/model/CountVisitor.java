1f50c169338fd975b22f7492780a56625266d967
Adit Krishnan <43497982+adit299@users.noreply.github.com>
2019-11-17 22:30:24 -0500

    added javadoc

--- Assignment2/src/ca/utoronto/utm/othello/model/CountVisitor.java
+++ Assignment2/src/ca/utoronto/utm/othello/model/CountVisitor.java
@@ -1,6 +1,14 @@
 package ca.utoronto.utm.othello.model;
 
 public class CountVisitor implements CVisitor {
+	
+	/**
+	 * Implementation of visitor to count player tokens
+	 * 
+	 * 
+	 * 
+	 * @param OthelloBoard, Player
+	 */
 
 	@Override
 	public int visit(OthelloBoard ob, char player) {
